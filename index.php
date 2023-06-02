7f37c6db7a34f8f479c71754fd6cd1961000d12c
vishwaja <vishwajadyapa1994@gmail.com>
2023-06-02 22:34:23 +0530

    declared

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <html>
  <head>
   <title>PHP Test</title>
+
  </head>
  <body>
+ <?php echo '<p> i love you naresh-from vishwaja</p>'; ?> 
+
  <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
